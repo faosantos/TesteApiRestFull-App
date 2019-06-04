15535a7baf86dd75dd0913e97d85ac78f099089c
faosantos <faosantos@restinga.ifrs.edu.br>
2019-06-04 16:48:03 -0300

    commit

--- teste_App/App.js
+++ teste_App/App.js
@@ -1,32 +1,3 @@
-import React from 'react';
-import { AsyncStorage } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux'
-import { persistStore } from 'redux-persist'
-import immutableTransform from 'redux-persist-transform-immutable'
-import store from './App/Store'
-import Root from './App/Root'
+import App from './app/index';
 
-
-
-export default class App extends React.Component {
-  componentDidMount(){
-    persistStore(store, {storage:AsyncStorage, transforms: [immutableTransform()],whitelist:['auth']} )
-  }
-  render() {
-    return (
-      <Provider store={store}>
-        <Root /> 
-      </Provider>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- teste_App/App/Config/URLs.js
+++
@@ -1,5 +0,0 @@
-// const BASE_URL = 'http://localhost:8000'
-const BASE_URL = 'http://192.168.0.34:8000'
-const API_URL = '/api'
-export const LOGIN_URL = BASE_URL + '/oauth/token'
-export const USER_URL = BASE_URL + API_URL + '/user'
